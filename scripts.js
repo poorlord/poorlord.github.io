a70af5c6dd629e199c2b565b3d4e3610d2103d0a
poorlord <poorlord@me.com>
2024-06-30 23:58:20 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,35 @@
+/* scripts.js */
+window.addEventListener('scroll', function() {
+    let scrollPosition = window.pageYOffset;
+    if (scrollPosition % 100 === 0) { // Adjust this value to control the frequency of falling elements
+        createFallingElement();
+    }
+});
+
+function createFallingElement() {
+    let element = document.createElement('div');
+    element.className = 'falling-element';
+    element.style.left = `${Math.random() * window.innerWidth}px`;
+    element.style.top = '0px';
+
+    document.body.appendChild(element);
+
+    let fallInterval = setInterval(function() {
+        let top = parseInt(window.getComputedStyle(element).top);
+        if (top < window.innerHeight - 20) {
+            element.style.top = `${top + 5}px`;
+        } else {
+            clearInterval(fallInterval);
+            pileUpElement(element);
+        }
+    }, 20);
+}
+
+function pileUpElement(element) {
+    let left = parseInt(window.getComputedStyle(element).left);
+    if (left < window.innerWidth / 2) {
+        element.style.left = '0px';
+    } else {
+        element.style.left = `${window.innerWidth - 20}px`;
+    }
+}
